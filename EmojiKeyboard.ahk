8eb989677f61ea83f16911b38c0be4aaf3898638
edabonacci <edabonacci@gmail.com>
2016-04-10 23:55:43 -0400

    Repository Creation
    Creation of the repository, committing v1.0 of Programmable Emoji
    Keyboard.

---
+++ EmojiKeyboard.ahk
@@ -0,0 +1,118 @@
+﻿#NoEnv  ; Recommended for performance and compatibility with future AutoHotkey releases.
+; #Warn  ; Enable warnings to assist with detecting common errors.
+SendMode Input  ; Recommended for new scripts due to its superior speed and reliability.
+SetWorkingDir %A_ScriptDir%  ; Ensures a consistent starting directory.
+#SingleInstance force
+OnExit, ExitSave ;Label to activate when the script is ending
+
+Run, LuaMacros.exe EmojiKeyboardMacro.lua -r ;runs LuaMacros with the appropriate .lua file, auto-running the file as soon as the application loads
+
+global emojiList := {}
+
+emojiList := loadArray("EmojiListSaved.txt") ;Loads associations from file into array 'emojiList'
+
+MsgBox Emoji Keyboard Initialized.
+
+;F24 - Run key in "KeyPressed.txt"
+;F23 - Ctrl
+;F22 - Enter
+;F21 - Shift
+
+F24:: ;Activate keys
+FileRead, key, keypressed.txt
+Tippy(emojiList[key])
+if emojiList[Key]
+{
+readyEmoji := emojiList[Key]
+Send %readyEmoji%
+}
+return
+
+^F24:: ;Program keys
+FileRead, key, keypressed.txt
+AddToEmojiList(key)
+;MsgBox % "Added: " key ": " emojiList[key]
+return
+
++F21::saveArray() ;Save associations
+
+^+F21::loadArray("EmojiListSaved.txt") ;Load associations
+
+~F22::Send, {Enter} ;Workaround to restore normal function of enter key
+
+Tippy(tipsHere, wait:=333) ;Makes a tooltip appear by the cursor
+{
+ToolTip, %tipsHere%
+SetTimer, noTip, %wait% ;--in 1/3 seconds by default, remove the tooltip
+}
+noTip:
+	ToolTip,
+	;removes the tooltip
+return
+
+AddToEmojiList(keyAssign) ;Adds the currently highlighted text to the list of associations, assigning it to keyAssign 
+{
+global emojiList
+clipStorage := ClipboardAll
+clipboard =
+Send, ^c
+ClipWait, 0
+if ErrorLevel
+{
+    MsgBox, Adding emoji failed!
+    return
+}
+emoji = %clipboard%
+;MsgBox %clipboard%
+;MsgBox %emoji%
+emojiList[keyAssign] := emoji
+clipboard := clipStorage
+clipStorage =
+emoji = 
+}
+
+genTextArray() ;Converts the array that holds the associations into text form for saving
+{
+global emojiList
+fileCache =
+For key, value in emojiList
+{
+	;MsgBox % key A_Tab value
+	fileCache := fileCache . key A_Tab value . "`n"
+}
+return fileCache
+}
+
+
+loadArray(fileName) ;Loads associations from fileName
+{
+arrayCache := {}
+	Loop, read, %fileName%
+	{
+		parseArray := StrSplit(A_LoopReadLine, A_Tab)
+		arrayCache[parseArray[1]] := parseArray[2]
+		;MsgBox % parseArray[1]
+		;MsgBox % parseArray[2]
+		;MsgBox % arrayCache[parseArray[1]]
+	}
+return arrayCache
+}
+
+saveArray() ;Saves associations to EmojiListSaved.txt
+{
+textArray := genTextArray()
+FileDelete, EmojiListSaved.txt
+file := FileOpen("EmojiListSaved.txt", "rw","UTF-8")
+file.Write(textArray)
+file.close
+MsgBox Saved!
+return 0
+}
+
+;Esc::ExitApp ;debug hotkeys
+;F12::Reload
+
+ExitSave: ;Responsible for saving associations to file and closing LuaMacros when the script ends (See "OnExit, ExitSave" at beginning of script)
+saveArray()
+Process, Close, LuaMacros.exe
+ExitApp
---
+++ EmojiKeyboardMacro.lua
@@ -0,0 +1,151 @@
+-- get luamacros here: http://www.hidmacros.eu/
+-- plug in your 2nd keyboard, load this script into LUAmacros, and press the triangle PLAY button.
+-- Then, press any key on that keyboard to assign logical name ('EMOJIKEYBOARDDEVICE') to macro keyboard
+--lmc_assign_keyboard('EMOJIKEYBOARDDEVICE');
+
+--The better solution: comment out the line above and uncomment the line below, replacing 'KEYBOARDIDREGEX' with the ID of your keyboard.
+--The ID of your desired keyboard can be found by running the script once, then checking the output of LuaMacros and seeing which ID corresponded with the name 'EMOJIKEYBOARDDEVICE'
+lmc_device_set_name('MACROS','F7150D6')
+
+--lmc_print_devices()
+
+lmc.minimizeToTray = true;
+lmc_minimize() --code responsible for minimizing LuaMacros into the taskbar
+
+sendToAHK = function (key)
+      print('It was assigned string:    ' .. key)
+      local file = io.open("KeyPressed.txt", "w") -- writing this string to a text file on disk is probably NOT the best method. Feel free to program something better!
+      file:write(key)
+      file:flush()
+      file:close()
+      lmc_send_keys('{F24}')  -- using the F24 key to trigger AutoHotKey is probably NOT the best method. Feel free to program something better!
+end
+
+local config = {
+	[45]  = "insert",
+	[36]  = "home",
+	[33]  = "pageup",
+	[46]  = "delete",
+	[35]  = "end",
+	[34]  = "pagedown",
+	[27]  = "escape",
+	[112] = "F1",
+	[113] = "F2",
+	[114] = "F3",
+	[115] = "F4",
+	[116] = "F5",
+	[117] = "F6",
+	[118] = "F7",
+	[119] = "F8",
+	[120] = "F9",
+	[121] = "F10",
+	[122] = "F11",
+	[123] = "F12",
+	[8]   = "backspace",
+	[220] = "backslash",
+	[13]  = "enter",
+	[16]  = "shift",
+	[17]  = "ctrl",
+	[38]  = "up",
+	[37]  = "left",
+	[40]  = "down",
+	[39]  = "right",
+	[32]  = "space",
+	[186] = "semicolon",
+	[222] = "singlequote",
+	[190] = "period",
+	[191] = "slash",
+	[188] = "comma",
+	[219] = "leftbracket",
+	[221] = "rightbracket",
+	[189] = "minus",
+	[187] = "equals",
+	[96]  = "num0",
+	[97]  = "num1",
+	[98]  = "num2",
+	[99]  = "num3",
+	[100] = "num4",
+	[101] = "num5",
+	[102] = "num6",
+	[103] = "num7",
+	[104] = "num8",
+	[105] = "num9",
+
+	[106] = "numMult",
+	[107] = "numDelete",
+	-- 108 is unknown...?
+	[109] = "numMult",
+	[110] = "numDelete",
+	[111] = "numDiv",
+	[144] = "numLock", --probably it is best to avoid this key. I keep numlock ON, or it has unexpected effects
+
+	[192] = "`",  --this is the tilde key just before the number row
+	[9]   = "tab",
+	[20]  = "capslock",
+	[18]  = "alt",
+
+
+	[string.byte('Q')] = "q",
+	[string.byte('W')] = "w",
+	[string.byte('E')] = "e",
+	[string.byte('R')] = "r",
+	[string.byte('T')] = "t",
+	[string.byte('Y')] = "y",
+	[string.byte('U')] = "u",
+	[string.byte('I')] = "i",
+	[string.byte('O')] = "o",
+	[string.byte('P')] = "p",
+	[string.byte('A')] = "a",
+	[string.byte('S')] = "s",
+	[string.byte('D')] = "d",
+	[string.byte('F')] = "f",
+	[string.byte('G')] = "g",
+	[string.byte('H')] = "h",
+	[string.byte('J')] = "j",
+	[string.byte('K')] = "k",
+	[string.byte('L')] = "l",
+	[string.byte('Z')] = "z",
+	[string.byte('X')] = "x",
+	[string.byte('C')] = "c",
+	[string.byte('V')] = "v",
+	[string.byte('B')] = "b",
+	[string.byte('N')] = "n",
+	[string.byte('M')] = "m",
+	[string.byte('0')] = "0",
+	[string.byte('1')] = "1",
+	[string.byte('2')] = "2",
+	[string.byte('3')] = "3",
+	[string.byte('4')] = "4",
+	[string.byte('5')] = "5",
+	[string.byte('6')] = "6",
+	[string.byte('7')] = "7",
+	[string.byte('8')] = "8",
+	[string.byte('9')] = "9",
+
+	--[255] = "printscreen" --these keys do not work
+}
+
+-- define callback for whole device
+lmc_set_handler('EMOJIKEYBOARDDEVICE', function(button, direction)
+	if (direction == 0) then return end  -- ignore up
+
+	if config[button] == "ctrl" then
+		lmc_send_keys('{F23}')
+		--print('Ctrl is Pressed')
+	elseif config[button] == "enter" then
+		lmc_send_keys('{F22}')
+		--print('Enter is Pressed')
+	elseif config[button] == "shift" then
+		lmc_send_keys('{F21}')
+		--print('Shift is Pressed')
+	elseif config[button] == "backspace" then
+		lmc_send_keys('{BACKSPACE}')
+	elseif type(config[button]) == "string" then
+		print(' ')
+		print('Your key ID number is:   ' .. button)
+		sendToAHK(config[button])
+	else
+		print(' ')
+		print('Not yet assigned: ' .. button)
+	end
+end)
